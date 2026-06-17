21875ccc1abb65aafa0ebfda5e79cbaf62e11ac3
Arev <arevg693@gmail.com>
2026-06-17 15:08:54 +0400

    add stack

---
+++ stack/index.js
@@ -0,0 +1,60 @@
+import { Deque } from "../deque/index.js"
+class Stack {
+  #data;
+  #size;
+
+  constructor(initialCapacity = 16) {
+    this.#data = new Deque(8);
+    this.#size = 0;
+  }
+
+  push(value) {  
+    this.#data. push_back(value);
+    this.#size++;
+  }
+
+  pop() {
+    this.#data.pop_back();
+    this.#size--;
+  }
+
+  peek() {
+    return this.#data.back();
+  }
+
+  size() {
+    return this.#size;
+  }
+
+  isEmpty() {
+    return this.#size === 0;
+  }
+
+  clear() {
+    this.#data.clear();
+    this.#size = 0;
+  }
+
+  toArray() {
+let arr = [];
+    return this.#data.toArray();
+  }
+
+  *[Symbol.iterator]() {
+        yield* this.#data;
+  }
+}
+
+let s1 = new Stack();
+s1.push(14);
+s1.push(17);
+s1.push(116);
+s1.pop();
+console.log(s1.isEmpty());
+console.log(s1.toArray());
+console.log(s1.size());
+for(let val of s1) {
+    console.log(val);
+}
+
+
